47061befe6de5e0c0d42982f346877c7236110fe
Pruthvi <pruthvi.kajavadra@gmail.com>
2023-07-25 11:55:59 +1200

    added display card for tv

--- src/app/search/SearchResultItem.tsx
+++ src/app/search/SearchResultItem.tsx
@@ -1,4 +1,4 @@
-import { movieGenres } from "@/clients/tmdb";
+import { GenresData, movieGenres } from "@/clients/tmdb";
 import {
   Box,
   Card,
@@ -7,6 +7,8 @@ import {
   CardHeader,
   Chip,
   CircularProgress,
+  CircularProgressClasses,
+  CircularProgressProps,
   Divider,
   Stack,
   Typography,
@@ -14,45 +16,70 @@ import {
 
 export const SearchResultItem = async ({
   title = "",
+  name = "",
   overview = "",
   media_type,
   release_date,
+  first_air_date,
   vote_average,
   genre_ids,
 }: {
   backdrop_path: string;
   id: number;
-  title: string;
+  title?: string;
+  name?: string;
   original_language: string;
-  original_title: string;
+  original_title?: string;
+  original_name?: string;
   overview: string;
   poster_path: string;
   media_type: string;
   genre_ids: Array<number>;
   popularity: number;
-  release_date: string;
+  release_date?: string;
+  first_air_date?: string;
   vote_average: number;
   vote_count: number;
 }) => {
-  if (media_type !== "movie") {
-    return <Typography>Search result is not a movie</Typography>;
+  let mediaTypeColor:CircularProgressProps["color"];
+  let mediaTitle;
+  let mediaDate;
+  let MediaChip = <></>;
+  let mediaGenres:GenresData;
+  switch(media_type) {
+    case "movie":
+      mediaTypeColor = "primary";
+      mediaTitle = title;
+      mediaDate = release_date;
+      MediaChip = <Chip label="Movie" variant="outlined" color={mediaTypeColor} />;
+      mediaGenres = await movieGenres();
+      break;
+    case "tv":
+      mediaTypeColor = "secondary";
+      mediaDate = first_air_date;
+      mediaTitle = name;
+      MediaChip = <Chip label="Tv" variant="outlined" color={mediaTypeColor} />;
+      mediaGenres = await movieGenres();
+      break;
+    default:
+
+      return <Typography>Search result is not a movie</Typography>;
   }
 
-  const { genres } = await movieGenres();
-
   const genreNames = genre_ids.map((genreId) => {
+    if(!mediaGenres) return;
     const { name } =
-      genres.find((currentGenre) => currentGenre.id === genreId) ?? {};
+    mediaGenres?.genres.find((currentGenre) => currentGenre.id === genreId) ?? {};
     return name;
   });
 
   return (
     <Card variant="outlined" sx={{ borderRadius: 2 }}>
       <CardHeader
-        title={<Typography variant="h5">{title}</Typography>}
+        title={<Typography variant="h5">{mediaTitle}</Typography>}
         subheader={
           <Typography variant="body2" color="text.secondary">
-            {new Date(release_date).toLocaleDateString()}
+            {(mediaDate && mediaDate!=="") && new Date(mediaDate).toLocaleDateString()}
           </Typography>
         }
         sx={{
@@ -63,7 +90,7 @@ export const SearchResultItem = async ({
             <CircularProgress
               variant="determinate"
               value={vote_average * 10}
-              color="primary"
+              color={mediaTypeColor}
               thickness={4}
             />
             <Box
@@ -88,7 +115,7 @@ export const SearchResultItem = async ({
       <CardContent>{overview}</CardContent>
       <CardContent>
         <Stack direction="row" justifyContent="space-between">
-          <Chip label="Movie" variant="outlined" color="primary" />
+          {MediaChip}
           <Stack
             divider={<Divider orientation="vertical" />}
             direction="row"
